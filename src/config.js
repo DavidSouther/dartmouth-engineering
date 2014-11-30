756bcb1e17f63b4f405718d5af9f6148a04a9f08
David Souther <davidsouther@gmail.com>
2014-11-29 19:11:06 -0500

    Fixed lints.

--- src/config.js
+++ src/config.js
@@ -1,9 +1,7 @@
-var Path = require('path');
-
 module.exports = function (config){
 
-    config.vendors.js.push("graphing/www/application.js");
-    config.vendors.js.push("graphing/www/templates.js");
+    config.vendors.js.push('graphing/www/application.js');
+    config.vendors.js.push('graphing/www/templates.js');
 
     return config;
 };
