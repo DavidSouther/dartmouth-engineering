abae2f0a08f2861111c2f2bd2dbfcf412295325b
David Souther <davidsouther@gmail.com>
2014-12-04 17:55:58 -0500

    Use published flaming-sansa.

--- src/config.js
+++ src/config.js
@@ -1,7 +1,9 @@
 module.exports = function (config){
 
-    config.vendors.js.push('graphing/www/application.js');
-    config.vendors.js.push('graphing/www/templates.js');
+    config.vendors.js.push('flaming-sansa/www/vendors.js');
+    config.vendors.js.push('flaming-sansa/www/application.js');
+    config.vendors.js.push('flaming-sansa/www/templates.js');
+    config.vendors.css.push('flaming-sansa/www/all.css');
 
     return config;
 };
